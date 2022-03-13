696f21aa8c25963bafdba48ad6667d9fbe611258
nefir2 <vadim.ahtamov@gmail.com>
2022-03-14 02:27:14 +0300

    rename, switch and minus 2 comments

--- MegaCalc.cs
+++ MegaCalc.cs
@@ -2,7 +2,7 @@
 using System.Text;
 namespace consoleCalc
 {
-	class Program
+	class MegaCalc
 	{
 		static readonly char[] массивЗнаков = { '+', '-', '*', '/', '^', '%', '!' }; //, ')', '(' 
 		static readonly char[] цифры = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
