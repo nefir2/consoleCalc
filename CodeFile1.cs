78a2269e34db06139ad89a29441441673c8d5d6e
nefir2 <vadim.ahtamov@gmail.com>
2022-03-14 00:16:19 +0300

    some test file for key input

---
+++ CodeFile1.cs
@@ -0,0 +1,23 @@
+﻿//using System;
+//namespace testing
+//{
+//    class TestFile
+//    {
+//        static void Main()
+//        {
+//            ConsoleKeyInfo клавиша;
+//            bool end = true;
+//            do
+//            {
+//                клавиша = Console.ReadKey(true);
+//                Console.Clear();
+//                Console.WriteLine($"нажатая клавиша:\n");
+//                Console.WriteLine($"переменная: {клавиша},");
+//                Console.WriteLine($"ключ: {клавиша.Key},");
+//                Console.WriteLine($"символ: {клавиша.KeyChar}.");
+//                Console.WriteLine($"дополнительная клавиша: {клавиша.Modifiers}");
+//                if (клавиша.Modifiers == ConsoleModifiers.Control && клавиша.Key == ConsoleKey.Enter) end = false;
+//            } while (end);
+//        }
+//    }
+//}
--- Program.cs
+++ Program.cs
@@ -2,112 +2,147 @@
 using System.Text;
 namespace consoleCalc
 {
-	class Program
-	{
-		static readonly char[] массивЗнаков = { '+', '-', '*', '/', '^', '%', '!'}; //, ')', '(' 
-		static readonly char[] цифры = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
-		static int количествоЗнаков = 0;
-		static void Main() 
-		{
-			string выражение;
-			do
-			{
-				Console.Write("доступные знаки: ");
-				ВыводМассиваЗнаков();
-				Console.Write("\nвведите выражение: ");
-				выражение = Нажатие_клавиш();
-				Console.WriteLine($"\n\nвы ввели: {выражение}\nколичество знаков: {количествоЗнаков}");
-				//Calc(выражение);
-				//Console.WriteLine($"\nответ: {Calc(выражение)}"); 
-			} while (выражение != "0");
-		}
-		static string Нажатие_клавиш()
-		{
-			StringBuilder ввод = new StringBuilder(null);
-			ConsoleKeyInfo клавиша;
-			bool exit;
-			do
-			{
-				клавиша = Console.ReadKey(true);
-				if (клавиша.Key == ConsoleKey.Backspace && ввод.Length > 0)
-				{
-					Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop);
-					Console.Write(' ');
-					for (int i = 0; i < массивЗнаков.Length; i++)
-					{
-						if (ввод.Length > 0 && ввод[^1] == массивЗнаков[i])
-						{
-							количествоЗнаков--;
-							break;
-						}
-					}
-					ввод.Remove(ввод.Length - 1, 1);
-					Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop);
-				}
-				for (int i = 0; i < цифры.Length; i++)
-				{
-					if (ввод.Length > 0 && ввод[^1] == '!') break;
-					else if (клавиша.KeyChar == цифры[i])
-					{
-						Console.Write(цифры[i]);
-						ввод.Append(клавиша.KeyChar);
-						break;
-					}
-				}
-				exit = false;
-				for (int i = 0; i < массивЗнаков.Length; i++)
-				{
-					if (ввод.Length == 0 && клавиша.KeyChar != '-') break;
-					for (int j = 0; j < массивЗнаков.Length; j++)
-					{
-						if (ввод.Length > 0 && ввод[^1] == '!' && клавиша.KeyChar == '!')
+    class Program
+    {
+        static readonly char[] массивЗнаков = { '+', '-', '*', '/', '^', '%', '!' }; //, ')', '(' 
+        static readonly char[] цифры = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+        static int количествоЗнаков = 0;
+        static void Main()
+        {
+            string выражение;
+            Console.Write("если ввести \"0\", то программа завершится.\n");
+            Console.Write("доступные знаки: ");
+            ВыводМассиваЗнаков();
+            do
+            {
+                Console.Write("\nвведите выражение: ");
+                выражение = Нажатие_клавиш();
+                Console.Clear();
+                if (выражение != "0") Console.WriteLine($"вы ввели: {выражение}\nколичество знаков: {количествоЗнаков}\n");
+                количествоЗнаков = 0;
+                //Calc(выражение);
+                //Console.WriteLine($"\nответ: {Calc(выражение)}"); 
+            } while (выражение != "0");
+        }
+        static string Нажатие_клавиш()
+        {
+            StringBuilder ввод = new StringBuilder(null);
+            ConsoleKeyInfo клавиша;
+            bool exit;
+            do
+            {
+                клавиша = Console.ReadKey(true);
+                Console.Clear();
+                Console.WriteLine($"\nнажатая клавиша:\n\nпеременная: {клавиша},\nключ: {клавиша.Key},\nсимвол: {клавиша.KeyChar}.\n");
+                if (клавиша.Key == ConsoleKey.Backspace && ввод.Length > 0)
+                {
+                    Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop);
+                    Console.Write(' ');
+                    for (int i = 0; i < массивЗнаков.Length; i++)
+                    {
+                        if (ввод.Length > 0 && ввод[^1] == массивЗнаков[i])
                         {
-							exit = true;
-							break;
+                            количествоЗнаков--;
+                            break;
                         }
-						if (ввод.Length > 0 && ввод[^1] == массивЗнаков[j] && ввод[^1] != '!') //если прошлый символ равен знаку - выход из цикла ввода знака
-						{//C# упростил: ввод[ввод.Length - 1]
-							exit = true;
-							break; 
-						}
-					}
-					if (exit) break;
-					if (клавиша.KeyChar == массивЗнаков[i])
-					{
-						Console.Write(массивЗнаков[i]);
-						ввод.Append(клавиша.KeyChar);
-						количествоЗнаков++;
-						break;
-					}
-				}
-			} while (клавиша.Key != ConsoleKey.Enter);
-			return ввод.ToString();
-		}
-		static void ВыводМассиваЗнаков()
-		{
-			for (int i = 0; i < массивЗнаков.Length; i++) 
-			{ 
-				Console.Write(массивЗнаков[i]); 
-				if (массивЗнаков.Length - 1 != i) Console.Write(", ");
-				else Console.Write(".");
-			}
-		}
-		static void Calc(string выр)
-		{
-			bool brek = false;
+                    }
+                    ввод.Remove(ввод.Length - 1, 1);
+                    Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop);
+                }
+                for (int i = 0; i < цифры.Length; i++)
+                {
+                    if (ввод.Length > 0 && ввод[^1] == '!') break;
+                    else if (клавиша.KeyChar == цифры[i])
+                    {
+                        Console.Write(цифры[i]);
+                        ввод.Append(клавиша.KeyChar);
+                        break;
+                    }
+                }
+                exit = false;
+                for (int i = 0; i < массивЗнаков.Length; i++)
+                {
+                    if (ввод.Length == 0 && клавиша.KeyChar != '-') break;
+                    for (int j = 0; j < массивЗнаков.Length; j++)
+                    {
+                        if (ввод.Length > 0 && ввод[^1] == '!' && клавиша.KeyChar == '!')
+                        {
+                            exit = true;
+                            break;
+                        }
+                        if (ввод.Length > 0 && ввод[^1] == массивЗнаков[j] && ввод[^1] != '!') //если прошлый символ равен знаку - выход из цикла ввода знака
+                        {//C# упростил: ввод[ввод.Length - 1]
+                            exit = true;
+                            break;
+                        }
+                    }
+                    if (exit) break;
+                    if (клавиша.KeyChar == массивЗнаков[i])
+                    {
+                        Console.Write(массивЗнаков[i]);
+                        ввод.Append(клавиша.KeyChar);
+                        количествоЗнаков++;
+                        break;
+                    }
+                }
+                /*
+				if (клавиша.Key == ConsoleKey.Enter)
+                {
+					for (int i = 0; i < массивЗнаков.Length; i++)
+                    {
+						if (ввод[^1] == массивЗнаков[i] && ввод[^1] != '!')
+                        {
+							клавиша = 0;
+                        }
+                    }
+                }
+				*/
+            } while (клавиша.Key != ConsoleKey.Enter);
+            return ввод.ToString();
+        }
+        static void ВыводМассиваЗнаков()
+        {
+            for (int i = 0; i < массивЗнаков.Length; i++)
+            {
+                Console.Write(массивЗнаков[i]);
+                if (массивЗнаков.Length - 1 != i) Console.Write(", ");
+                else Console.Write(".");
+            }
+        }
+        static void Calc(string выр)
+        {
+            /*
 			for (int i = 0; i < выр.Length; i++)
-			{
-				for (int j = 0; j < массивЗнаков.Length; j++)
-				{
-					if (выр[i] != 0 && выр[i] == массивЗнаков[j])
-					{
-						brek = true;
-						break;
-					}
-				}
-				if (brek) break;
-			}
-		}
+            {
+				if (выр[i] == '*' || выр[i] == '/' || выр[i] == '%')
+            }
+			*/
+            //определение границ в виде знаков первого числа
+            int znak1, znak2;
+            bool brek = false; //булевая переменная для выхода из второго цикла
+            if (выр[0] == '-') znak1 = 0; //если первый знак - минус
+            else if (выр[0] != '-')
+            {//если первый знак не минус, то начинаем поиск
+                for (int i = 1; i < выр.Length; i++)
+                {//проход по массиву выражения со второго символа, т.к. на первом месте может быть только минус (из функции "нажатие_клавиш")
+                    for (int j = 0; j < массивЗнаков.Length; j++)
+                    {//проход по массиву знаков
+                        if (выр[i] == массивЗнаков[j])
+                        {//если нашёлся знак, то сохранение первого знака и выход из двух циклов
+                            znak1 = выр[i];
+                            brek = true; //выход со второго
+                            break; //выход с первого цикла
+                        }
+                    }
+                    if (brek) break; //выход со второго
+                }
+            }
+            else
+            {
+                znak1 = -1;
+                znak2 = выр.Length;
+            }
+        }
 
 
 
@@ -144,19 +179,19 @@ namespace consoleCalc
 
 
 
-		static double Факториал(int n)
-		{
-			double ans = 1;
-			for (int i = 2; i <= n; i++) ans *= i;
-			return ans;
-		}
-		static int Степень(int a, int b)
-		{
-			int ans = 1;
-			for (int i = 0; i < b; i++) ans *= a;
-			return ans;
-		}
-		/*
+        static double Факториал(int n)
+        {
+            double ans = 1;
+            for (int i = 2; i <= n; i++) ans *= i;
+            return ans;
+        }
+        static int Степень(int a, int b)
+        {
+            int ans = 1;
+            for (int i = 0; i < b; i++) ans *= a;
+            return ans;
+        }
+        /*
 		static double Calc(string выр)
 		{
 			double ans = 0;
@@ -220,5 +255,5 @@ namespace consoleCalc
 			return знак;
 		}
 		*/
-	}
+    }
 }
